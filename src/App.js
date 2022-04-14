5e8b754047c0d43874d58ab1682324f3cc06dffe
kabir <makkobir79@gmail.com>
2022-04-14 14:42:56 +0600

    react router emlimented

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Pages/Shared/Header/Header";
 import Footer from "./Pages/Shared/Footer/Footer";
+import Home from "./Pages/Home/Home/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </div>
   );
